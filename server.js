b72fee92795cffb4d2c9519df4769a519494561d
Richard <richard.anaya@gmail.com>
2012-04-16 02:23:50 -0500

    upgrading various pieces

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var port = process.env.PORT || 9999;
 var server = util.configureServer();
 
 server.get('/favicon.ico',
-    function(req, res) {
-        res.redirect('/images/favicon.ico');
+    function (req, res) {
+        res.redirect('/img/favicon.ico');
     }
 );
 
 server.get(/^.*$/,
-    function(req, res) {
-        res.render('index.jinjs',{ name: "Richard", languages: [{name:"English",code:"en"},{name:"Spanish",code:"es"}] });
+    function (req, res) {
+        res.render('index.jinjs', { name:"Richard" });
     }
 );
 
